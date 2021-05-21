8e950082f9bea80952c271061a5118ce60cbecf0
Tatyana Alexander <tatyana.theresa@gmail.com>
2021-05-20 20:21:55 -0400

    added scripts

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,11 @@
 "use strict";
+
+let cTemp = 100;
+let fTemp = (cTemp * 1.8000) + 32;
+
+console.log("Fahrenheit is " + fTemp);
+
+let fTemp = 212;
+let cTemp = (fTemp-32)*5/9;
+
+console.log("Celcius is " + cTemp);
